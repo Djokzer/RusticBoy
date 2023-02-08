f8f64a5d35806f047838f83a8e575b36f91e1e5e
abivarma.kandiah <abivarman.kandiah@etu.hesge.ch>
2023-02-08 19:18:55 +0100

    Create basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::{prelude::*, miniquad::conf, window};
+
+const SIZE : u16 = 1024;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "RusticBoy".to_owned(),
+        window_height: SIZE as i32,
+        window_width : SIZE as i32,
+        ..Default::default()
+    }
+}
+
+
+#[macroquad::main(window_conf)]
+async fn main() 
+{
+    loop {
+        //CLEAR
+        clear_background(BLACK);
+
+        next_frame().await
+    }
 }
